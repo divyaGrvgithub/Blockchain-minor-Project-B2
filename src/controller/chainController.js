5e41c2eb6bc1ecf521419835df12a92e2fdf905e
Divyamala Tiwari <119675338+divyaGrvgithub@users.noreply.github.com>
2023-01-22 23:08:53 +0530

    Update chainController.js

--- src/controller/chainController.js
+++ src/controller/chainController.js
@@ -1,23 +1,48 @@
 const axios = require('axios');
 const chainModel = require("./model/chainModel")
 
-const getBlockChain = async (req, res) => {
+const getBlockChain = async function (req, res) {
     try {
         let option = {
             method: "get",
-            url: "https://docs.coincap.io/v2/assets",
+            url: "https://api.coincap.io/v2/assets",
+            headers: {
+                Authorization: "Bearer 01213021-558e-4a5d-a90f-9483f204067b"
+            }
+        }
 
-        };
-        let result = await axios(option);
-        console.log(result);
-        let data = result.data.data;
-        const sortvalue = data.sort((a, b) => { return a.changePercent24Hr - b.changePercent24Hr })
-        await chainModel.deleteMany()
-        const savedData = await chainModel.insertMany(sortvalue)
-        res.status(201).send({ status: true, message: sortvalue });
-    } catch (error) {
-        console.log(error);
-        res.status(500).send({ message: error.message });
+        let data = await axios(option);
+        let coinData = data.data;
+        let checkCoinData = await coinModel.find();
+
+        if (checkCoinData.length == 0) {
+
+            for (let i = 0; i < coinData.data.length; i++) {
+                let newObj = {
+                    symbol: coinData.data[i].symbol,
+                    name: coinData.data[i].name,
+                    marketCapUsd: coinData.data[i].marketCapUsd,
+                    priceUsd: coinData.data[i].priceUsd
+                }
+                await chainModel.create(newObj);
+            }
+            return res.status(201).send({ status: true, message: "CoinCap has created." });
+
+        } else {
+
+            let getCoinsData = await chainModel.find().select({ _id: 0, __v: 0 });
+            for (let i = 0; i < getCoinsData.length; i++) {
+                getCoinsData[i]._doc.changePercent24Hr = coinData.data[i].changePercent24Hr;
+            }
+
+            for (let i = 0; i < getCoinsData.length; i++) {
+                getCoinsData.sort((a, b) => (b.changePercent24Hr) - (a.changePercent24Hr));
+            }
+            return res.status(200).send({ "data": getCoinsData });
+
+        }
+    } catch (err) {
+        return res.status(500).send({ status: false, msg: err.message });
     }
 }
 
